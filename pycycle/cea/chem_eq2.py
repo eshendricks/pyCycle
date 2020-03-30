derivative fixes

pycycle/cea/chem_eq2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import openmdao.api as om
- 
+
 from pycycle.constants import P_REF, R_UNIVERSAL_ENG, MIN_VALID_CONCENTRATION
 
 class ChemEq2(om.ImplicitComponent):
@@ -22,44 +22,51 @@ class ChemEq2(om.ImplicitComponent):
         # self.add_input('P', val=1.0*np.ones(nn), units="bar", desc="Pressure")
         # self.add_input('T', val=400.*np.ones(nn), units="degK", desc="Temperature")
 
-        self.add_input('b0', val=np.ones((num_elements,nn)),
+        self.add_input('b0', val=np.ones((nn, num_elements)),
                         desc='assigned kg-atoms of element i per total kg of reactant '
                              'for the initial prod amounts')
-        
-        self.add_input('mu', val=np.ones((num_prods,nn)), units=None) #Fix units
 
-        self.add_output('pi', val=np.ones((num_elements,nn)),
+        self.add_input('mu', val=np.ones((nn, num_prods)), units=None) #Fix units
+
+        self.add_output('pi', val=np.ones((nn, num_elements)),
                         desc="modified lagrange multipliers from the Gibbs lagrangian")
 
-        self.add_output('n', val=np.ones((num_prods,nn)),
+        self.add_output('n', val=np.ones((nn, num_prods)),
                         desc="mole fractions of the mixture",
                         lower=0.,
                         res_ref=10000.
                         )
 
-        # Replace the partial derivatives with analytic calculations
-        self.declare_partials('pi','n', method='cs')
-        self.declare_partials('pi','b0', method='cs')
-        self.declare_partials('n','mu', method='cs')
-        self.declare_partials('n','pi', method='cs')
+        rows = np.repeat(np.arange(num_elements * nn), num_prods)
+        col = np.tile(np.arange(num_prods), num_elements)
+        cols = np.tile(col, nn) + np.repeat(num_prods * np.arange(nn), num_elements * num_prods)
+        val = np.tile(thermo_data.aij.flatten(), nn)
 
-        # ar_elems = np.arange(num_elements)
-        # ar_prods = np.arange(num_prods)
-        # self.declare_partials('pi','n', val=thermo_data.aij)
-        # self.declare_partials('pi','b0', rows=ar_elems, cols=ar_elems, val=-1.)
-        # # self.declare_partials('pi','pi')
-        # self.declare_partials('n','mu', rows=ar_prods, cols=ar_prods, val=1.)
-        # self.declare_partials('n','pi', val=-thermo_data.aij.T)
+        self.declare_partials('pi', 'n', rows=rows, cols=cols, val=val)
+
+        row_col = np.arange(nn * num_elements)
+        self.declare_partials('pi', 'b0', rows=row_col, cols=row_col, val=-1.0)
+
+        row_col = np.arange(nn * num_prods)
+        self.declare_partials('n', 'mu', rows=row_col, cols=row_col, val=1.0)
+
+        rows = np.repeat(np.arange(num_prods * nn), num_elements)
+        col = np.tile(np.arange(num_elements), num_prods)
+        cols = np.tile(col, nn) + np.repeat(num_elements * np.arange(nn), num_elements * num_prods)
+        val = -np.tile(thermo_data.aij.T.flatten(), nn)
+
+        self.declare_partials('n', 'pi', rows=rows, cols=cols, val=val)
 
     def guess_nonlinear(self, inputs, outputs, residuals):
         thermo_data = self.options['thermo_data']
         aij = thermo_data.aij
 
+        aij_inv = np.linalg.inv(np.matmul(aij,aij.T))
         # Compute initial guesses using left and right inverses of aij
-        inv_right = np.matmul(aij.T,np.linalg.inv(np.matmul(aij,aij.T)))
-        outputs['n'] = np.matmul(inv_right,inputs['b0'])
-        inv_left = np.matmul(np.linalg.inv(np.matmul(aij,aij.T)),aij)
-        outputs['pi'] = np.matmul(inv_left,inputs['mu'])
+        inv_right = np.matmul(aij.T, aij_inv)
+        outputs['n'] = np.einsum('ij,kj->ki', inv_right, inputs['b0'])
+        inv_left = np.matmul(aij_inv, aij)
+        outputs['pi'] = np.einsum('ij,kj->ki', inv_left, inputs['mu'])
 
         # print(outputs['n'])
         # print(outputs['pi'])
@@ -67,7 +74,8 @@ class ChemEq2(om.ImplicitComponent):
     def apply_nonlinear(self, inputs, outputs, resids):
         thermo_data = self.options['thermo_data']
 
-        resids['pi'] = np.matmul(thermo_data.aij, outputs['n']) - inputs['b0']
-        resids['n'] = inputs['mu'] - np.matmul(thermo_data.aij.T,outputs['pi'])
+        resids['pi'] = np.einsum('ij,kj->ki', thermo_data.aij, outputs['n']) - inputs['b0']
+        resids['n'] = inputs['mu'] - np.einsum('ji,kj->ki', thermo_data.aij, outputs['pi'])
 
-    # def linearize(self,inputs,outputs,J):
+        print(inputs['mu'], outputs['pi'], outputs['n'])
+

pycycle/cea/chem_potential.py
@@ -16,11 +16,11 @@ class Chem_Potential_Calcs(om.ExplicitComponent):
         num_prods = len(prod_list)
 
         self.add_input('P', val=np.ones(nn), units="bar", desc="Pressure")
-        self.add_input('n', val=np.ones((num_prods,nn)), desc="Mass fractions of products")
-        self.add_input('H0', val=np.ones((num_prods,nn)), units=None) #Fix units
-        self.add_input('S0', val=np.ones((num_prods,nn)), units=None) #Fix units
+        self.add_input('n', val=np.ones((nn, num_prods)), desc="Mass fractions of products")
+        self.add_input('H0', val=np.ones((nn, num_prods)), units=None) #Fix units
+        self.add_input('S0', val=np.ones((nn, num_prods)), units=None) #Fix units
 
-        self.add_output('mu', val=np.ones((num_prods,nn)), units=None) #Fix units
+        self.add_output('mu', val=np.ones((nn, num_prods)), units=None) #Fix units
 
         # Replace the partial derivatives with analytic calculations
         ar = np.arange(nn*num_prods)
@@ -36,7 +36,7 @@ class Chem_Potential_Calcs(om.ExplicitComponent):
         # set a lower value the concentration for computing mu
         n = np.where(inputs['n']>1e-10, inputs['n'], 1e-10)
 
-        outputs['mu'] = inputs['H0'] - inputs['S0'] + np.log(n) + np.log(inputs['P']/P_REF) - np.log(n_moles)
+        outputs['mu'] = inputs['H0'] - inputs['S0'] + np.log(n) + np.log(inputs['P']/P_REF)[..., np.newaxis] - np.log(n_moles)
 
 
     # def compute_partials(self, inputs, J):
@@ -82,8 +82,8 @@ class Chem_Potential(om.Group):
             self.add_subsystem('HS_'+prod, table, promotes_inputs=['T'])
 
         mux_comp = self.add_subsystem(name='mux', subsys=om.MuxComp(vec_size=len(prod_list)))
-        mux_comp.add_var('H0', shape=(nn,), axis=0, units=None)
-        mux_comp.add_var('S0', shape=(nn,), axis=0, units=None)
+        mux_comp.add_var('H0', shape=(nn,), axis=1, units=None)
+        mux_comp.add_var('S0', shape=(nn,), axis=1, units=None)
 
         for num, prod in enumerate(prod_list):
             self.connect('HS_%s.H0_T'%prod,'mux.H0_%i'%num)
@@ -124,6 +124,6 @@ if __name__ == "__main__":
     print('H0', prob['mux.H0'])
     print('S0', prob['mux.S0'])
     print('mu', prob['mu'])
-    
+
     prob.check_partials()
 